66853a1bfcbad0019f3fecbe1547673d3dbd3adb
jenaamj <jenaamj@gmail.com>
2024-02-01 18:41:56 +0530

    add here

---
+++ src/config.js
@@ -0,0 +1,6 @@
+//export const BACKEND_URL = "http://localhost:4000/api";
+export const BACKEND_URL = "https://backend-ra4k.onrender.com/api"
+// export const BACKEND_URL = "http://localhost:4000/api";
+
+// export const FRONTEND_URL = "https://magical-eclair-bcbda4.netlify.app";
+export const FRONTEND_URL = "https://soft-malasada-a0cb65.netlify.app";
