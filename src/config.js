7049dff8eb2bc1cb89a88e71732441e9f6d3aa2c
jenaamj <jenaamj@gmail.com>
2024-02-02 19:21:23 +0530

    add here 1

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ export const BACKEND_URL = "https://backend-ra4k.onrender.com/api"
 
 // export const FRONTEND_URL = "https://magical-eclair-bcbda4.netlify.app";
 // export const FRONTEND_URL = "https://soft-malasada-a0cb65.netlify.app";
-export const FRONTEND_URL = "https://frontened-six.vercel.app"
+export const FRONTEND_URL = "https://quizzie-mu.vercel.app"
